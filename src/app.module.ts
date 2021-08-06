b514b9e36817ac8a7cf9563f5d31cd6b3fbefaf4
Odilomar <odilomarjunior.contact@gmail.com>
2021-08-06 13:37:00 -0300

    feat(app): setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import { getConnectionOptions } from 'typeorm';
+import { UserModule } from './modules/user/user.module';
 
+config();
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), { autoLoadEntities: true }),
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
